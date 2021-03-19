8df8567e64ac9658cacb4439d4f25ab29c29ce5f
片寄奎汰 <kkatayose@sco.jp.net>
2021-03-19 18:04:27 +0900

    11

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -14,12 +14,18 @@ class PostsController < ApplicationController
         @post = Post.new(
             title: params[:title],
             content: params[:content],
-            user_id: 1
+            user_id: 2
            )
         if @post.save
           redirect_to("/posts/index")
         else
           render("posts/new")
         end
-     end
+
+    def destroy
+      @post = Post.find_by(id: params[:id])
+      @post.destroy
+      redirect_to("/posts/index")
+    end
   end
+end

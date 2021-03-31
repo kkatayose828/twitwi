6b8b103801570284e1ec943cbab23d67f3ee7631
片寄奎汰 <kkatayose@sco.jp.net>
2021-03-31 17:13:08 +0900

    16 ログインできるところまで

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,4 @@
 class PostsController < ApplicationController
-    before_action :ensure_correct_user,{only: [:destroy]}
-    before_action :set_current_user
     def index
       @posts = Post.all.order("created_at DESC").page(params[:page]).per(20)
     end
@@ -33,17 +31,5 @@ class PostsController < ApplicationController
       @post.destroy
       flash[:notice] = "スレッドを削除しました"
       redirect_to("/posts/index")
-    end
-
-    def ensure_correct_user
-      @post = Post.find_by(id: params[:id])
-      if @post.user_id != session[:user_id]
-        flash[:notice] = "権限がありません"
-        redirect_to("/posts/index")
-      end
-    end
-
-    def set_current_user
-      session[:user_id] = 3
     end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,10 +5,20 @@ class UsersController < ApplicationController
     def login
         @user = User.find_by(email: params[:email], password: params[:password])
         if @user
+          session[:user_id] = @user.id
           flash[:notice] = "ログインしました"
           redirect_to("/posts/index")
         else
+          @error_message = "メールアドレスまたはパスワードが間違っています"
+          @email =params[:email]
+          @password = params[:password]
           render("/users/login_form")
         end
+    end
+
+    def logout
+        session[:user_id] = nil
+        flash[:notice] = "ログアウトしました"
+        redirect_to("/login")
     end
 end

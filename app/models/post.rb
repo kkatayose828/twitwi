47f9a86b14e3a47d691734d6843bdb7ebdf7c6b3
片寄奎汰 <kkatayose@sco.jp.net>
2021-04-09 15:54:52 +0900

    20 find_byから belong_to has_many リレーションに変更、コメント一覧　

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,11 +1,6 @@
 class Post < ApplicationRecord
     validates :content, {presence: true, length: {maximum: 1000}}
     validates :user_id, {presence: true}
-  def user  
-    return User.find_by(id: self.user_id)
-  end
-
-  def posts
-    return Post.where(user_id: self.id)
-  end
+    has_many :comments
+    belongs_to :user
 end
